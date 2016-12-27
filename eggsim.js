0a500d022562af5af257bb3d38a80ccaadbf812a
Gabriel Lopez <gabiota3000@yahoo.com>
2016-12-27 16:03:29 -0500

    Add files via upload

---
+++ eggsim.js
@@ -0,0 +1,139 @@
+var imgP = [
+  ["http://i.imgur.com/LDgpVum.png","You recieved Godess's Lament x3!",13400111],
+		["http://i.imgur.com/LDgpVum.png","You recieved Godess's Lament x5!",13400111],
+		["http://i.imgur.com/O97RowN.png","You recieved Quick HP Gain Potion x25!", 0],
+		["http://i.imgur.com/O97RowN.png","You recieved Quick HP Gain Potion x50!", 0],
+		["http://i.imgur.com/skSppj5.png","You recieved Quick MP Gain Potion x25!", 0],
+		["http://i.imgur.com/skSppj5.png","You recieved Quick MP Gain Potion x50!", 0],
+		["http://i.imgur.com/mXIqbUQ.png","You recieved Heavy Energy Potion x10!", 0],
+		["http://i.imgur.com/mXIqbUQ.png","You recieved Heavy Energy Potion x15!", 0],
+		["http://i.imgur.com/URXgoK4.png","You recieved Cure Potion x10!", 0],
+		["http://i.imgur.com/URXgoK4.png","You recieved Cure Potion x15!", 0],
+		["http://i.imgur.com/VgmjEgV.png","You recieved Super Attack Potion x10!", 0],
+		["http://i.imgur.com/VgmjEgV.png","You recieved Super Attack Potion x15!", 0],
+		["http://i.imgur.com/yZzYCwd.png","You recieved Super Defense Potion x10!", 0],
+		["http://i.imgur.com/yZzYCwd.png","You recieved Super Defense Potion x15!", 0],
+		["http://i.imgur.com/CisBmJQ.png","You recieved Super Paralyze Potion x10!", 0],
+		["http://i.imgur.com/CisBmJQ.png","You recieved Super Paralyze Potion x15!", 0],
+		["http://i.imgur.com/DWeSrTz.png","You recieved Super Critical Potion x10!", 0],
+		["http://i.imgur.com/DWeSrTz.png","You recieved Super Critical Potion x15!", 0],
+		["http://i.imgur.com/xsTizkx.png","You recieved Super Stun Potion x10!", 0],
+		["http://i.imgur.com/xsTizkx.png","You recieved Super Stun Potion x15!", 0],
+		["http://i.imgur.com/u7Qmg7m.png","You recieved Super Speed Potion x10!", 0],
+		["http://i.imgur.com/u7Qmg7m.png","You recieved Super Speed Potion x15!", 0],
+		["http://i.imgur.com/1oJqmyy.png","You recieved Yellow Courier Bird x5!", 0],
+		["http://i.imgur.com/1oJqmyy.png","You recieved Yellow Courier Bird x10!", 0],
+		["http://i.imgur.com/8iYcdzH.png","You recieved Blue Courier Bird x5!", 0],
+		["http://i.imgur.com/8iYcdzH.png","You recieved Blue Courier Bird x10!", 0],
+		["http://i.imgur.com/dcCQiuC.png","You recieved Dreamy Approval Stamp x5!", 0],
+		["http://i.imgur.com/dcCQiuC.png","You recieved Dreamy Approval Stamp x30!", 0],
+		["http://i.imgur.com/dcCQiuC.png","You recieved Dreamy Approval Stamp x50!", 0],
+		["http://i.imgur.com/dcCQiuC.png","You recieved Dreamy Approval Stamp x100!", 0],
+		["http://i.imgur.com/TpcX255.png","You recieved Dreamy Item Protection Magic Jelly x5!", 0],
+		["http://i.imgur.com/TpcX255.png","You recieved Dreamy Item Protection Magic Jelly x30!", 0],
+		["http://i.imgur.com/TpcX255.png","You recieved Dreamy Item Protection Magic Jelly x50!", 0],
+		["http://i.imgur.com/TpcX255.png","You recieved Dreamy Item Protection Magic Jelly x100!", 0],
+		["http://i.imgur.com/TpcX255.png","You recieved Dreamy Item Protection Magic Jelly x200!", 0],
+		["http://i.imgur.com/0VTZUmH.png","You recieved Magnifying Glass of Fantasy x5!", 0],
+		["http://i.imgur.com/0VTZUmH.png","You recieved Magnifying Glass of Fantasy x10!", 0],
+		["http://i.imgur.com/yinA4Ck.png","You recieved Scroll of Unlimited Learning!", 0],
+		["http://i.imgur.com/MRMGYW9.png","You recieved Portable Enhancement Kit!", 0],
+		["http://i.imgur.com/EFNzBFo.png","You recieved Portable Crafting Kit!", 0],
+		["http://i.imgur.com/5lIWikT.png","You recieved Portable Repair Kit!", 0],
+		["http://i.imgur.com/yDzozxO.png","You recieved Hair Dye Box!", 5804481],
+		["http://i.imgur.com/yDzozxO.png","You recieved Skin Treatment Box!", 5804481],
+		["http://i.imgur.com/yDzozxO.png","You recieved Contact Lens Box!", 5804481],
+		["http://i.imgur.com/L8rKzAe.png","You recieved Lucky Star Earrings (HP)!", 0],
+		["http://i.imgur.com/L8rKzAe.png","You recieved Lucky Star Earrings (MP)!", 0],
+		["http://i.imgur.com/Ui9FqrE.png","You recieved Adamant Tiger Medalion!", 6363561],
+		["http://i.imgur.com/g1LxF9b.png","You recieved Dark Archangel Wings!", 0],
+		["http://i.imgur.com/CPbZv24.png","You recieved White Fairy Wings!", 4871440],
+		["http://i.imgur.com/y20H3Z4.png","You recieved Emerald Fairy Wings!", 4871440],
+		["http://i.imgur.com/FpSLRqk.png","You recieved Lavender Fairy Wings!", 4871440],
+		["http://i.imgur.com/LDgpVum.png","You recieved Godess's Lament x3!",13400111],
+		["http://i.imgur.com/jaRAnoo.png","You recieved Pure White Giant Feather Wings!",13400111],
+		["http://i.imgur.com/elB0Sx0.png","You recieved Pure White Giant Feather Decal!",13400111],
+		["http://i.imgur.com/f31qJQp.png","You recieved Pure White Giant Feather Tail!",13400111],
+		["http://i.imgur.com/3yWQxEt.png","You recieved Right Cheek Bandage Decal!", 0],
+		["http://i.imgur.com/1mMEghS.png","You recieved Heart Decal!", 4871440],
+		["http://i.imgur.com/9G28GGe.png","You recieved Spade Decal!", 4871440],
+		["http://i.imgur.com/X5cxbV8.png","You recieved Yellow Tail (Physical)!", 4871440],
+		["http://i.imgur.com/X5cxbV8.png","You recieved Yellow Tail (Magical)!", 4871440],
+		["http://i.imgur.com/gqTw5cz.png","You recieved White Tail (Physical)!", 4871440],
+		["http://i.imgur.com/gqTw5cz.png","You recieved White Tail (Magical)!", 4871440],
+		["http://i.imgur.com/MfLwiN7.png","You recieved Blue Tail (Physical)!", 4871440],
+		["http://i.imgur.com/MfLwiN7.png","You recieved Blue Tail (Magical)!", 4871440],
+		["http://i.imgur.com/TmS0qTj.png","You recieved Black Hamster Tail (Physical)!", 4871440],
+		["http://i.imgur.com/TmS0qTj.png","You recieved Black Hamster Tail (Magical)!", 4871440],
+		["http://i.imgur.com/7zgdOVQ.png","You recieved Shiny Star Fragment Ring (STR)!", 4871440],
+		["http://i.imgur.com/7zgdOVQ.png","You recieved Shiny Star Fragment Ring (AGI)!", 4871440],
+		["http://i.imgur.com/7zgdOVQ.png","You recieved Shiny Star Fragment Ring (INT)!", 4871440],
+		["http://i.imgur.com/7zgdOVQ.png","You recieved Shiny Star Fragment Ring (VIT)!", 4871440],
+		["http://i.imgur.com/nS22XG3.png","You recieved Gentle Butterfly Spirit!", 4871440],
+		["http://i.imgur.com/XjyaKWB.png","You recieved Innocent Iona Spirit!", 5804481],
+		["http://i.imgur.com/HByHqA5.png","You recieved Red Cross Earrings (HP)!", 4871440],
+		["http://i.imgur.com/93tJ1lI.png","You recieved Green Cross Earrings (HP)!", 4871440],
+		["http://i.imgur.com/InFI0pT.png","You recieved Blue Cross Earrings (HP)!", 4871440],
+		["http://i.imgur.com/HByHqA5.png","You recieved Red Cross Earrings (MP)!", 4871440],
+		["http://i.imgur.com/93tJ1lI.png","You recieved Green Cross Earrings (MP)!", 4871440],
+		["http://i.imgur.com/InFI0pT.png","You recieved Blue Cross Earrings (MP)!", 4871440],
+		["http://i.imgur.com/CdgnJMR.png","You recieved Shiny Emerald Black Dragon Ring!", 5804481],
+		["http://i.imgur.com/eRyJgd9.png","You recieved Shiny Emerald Black Dragon Earrings!", 5804481],
+		["http://i.imgur.com/lICFnmU.png","You recieved Shiny Emerald Black Dragon Necklace!", 5804481],
+		["http://i.imgur.com/HmVUmTp.png","You recieved Gold Dragon's Flame Ring!", 13400111],
+		["http://i.imgur.com/x764Quh.png","You recieved Gold Dragon's Flame Earrings!", 13400111],
+		["http://i.imgur.com/FL8jjpB.png","You recieved Gold Dragon's Flame Necklace!", 13400111],
+		["http://i.imgur.com/pOpTbsO.png","You recieved Cheery Necklace!", 4871440],
+		["http://i.imgur.com/G7B7OTc.png","You recieved (Merc) Iona!", 6363561],
+		["http://i.imgur.com/G7B7OTc.png","You recieved (Merc) Rampaging Jasmine!", 6363561],
+		["http://i.imgur.com/Jowu8EX.png","You recieved (Exploration) Lucky Coin Pouch x50!", 5804481],
+		["http://i.imgur.com/Jowu8EX.png","You recieved (Exploration) Lucky Coin Pouch x100!", 5804481],
+		["http://i.imgur.com/6kyf8y7.png","You recieved (Exploration) Mercenary Awakening Potion (Small) Pouch x5!", 5804481],
+		["http://i.imgur.com/hzI0tda.png","You recieved (Exploration) Mercenary Awakening Potion (Medium) Pouch x1!", 13400111],
+		["http://i.imgur.com/hzI0tda.png","You recieved (Exploration) Mercenary Awakening Potion (Medium) Pouch x3!", 13400111],
+		["http://i.imgur.com/hzI0tda.png","You recieved (Exploration) Mercenary Awakening Potion (Medium) Pouch x5!", 13400111],
+		["http://i.imgur.com/tfKUyGE.png","You recieved (Exploration) Mercenary Awakening Potion (Large) Pouch x1!", 6363561],
+		["http://i.imgur.com/tfKUyGE.png","You recieved (Exploration) Mercenary Awakening Potion (Large) Pouch x3!", 6363561],
+		["http://i.imgur.com/tfKUyGE.png","You recieved (Exploration) Mercenary Awakening Potion (Large) Pouch x5!", 6363561],
+		["http://i.imgur.com/KNf2CCz.png","You recieved Costume Hybrid Tablet!", 0],
+		["http://i.imgur.com/rffpKr3.png","You recieved Epic Synthesis Weapon Coupon!", 6363561],
+		["http://i.imgur.com/t8SuLpQ.png","You recieved Warranty!", 0],
+		["http://i.imgur.com/jvPlqG5.png","You recieved Ally EXP Juice x20!", 4871440],
+		["http://i.imgur.com/jvPlqG5.png","You recieved Ally EXP Juice x50!", 4871440],
+		["http://i.imgur.com/jvPlqG5.png","You recieved Ally EXP Juice x100!", 4871440]
+  ]
+
+
+var imgS = []
+function loadS(){
+for(var i=0; i<imgP.length; i++){
+  imgS[i] = new Image();
+  imgS[i].src = imgP[i][0];
+}}
+var imgCount = 0;
+
+function egg(){
+  imgCount += 1;
+  console.log(imgCount);
+  var r = Math.floor(Math.random() * imgP.length);
+  document.getElementById('myImage').src = imgS[r];
+  if(imgCount >= 40){
+    clearInterval(rInt);
+    var r = Math.floor(Math.random() * imgP.length);
+    document.getElementById('res').innerHTML = imgP[r][1]
+    console.log('hello');
+    document.getElementById('myImage').src = imgS[r];
+    console.log('bye');
+    imgCount = 0;
+  }
+  if(imgCount==39){
+    clearInterval(rInt);
+    rInt = setInterval('egg()', 1500);
+  }
+}
+function eggify(){
+  loadS()
+  console.log("start");
+  rInt = setInterval('egg()', 80);
+  }
+
